265fd9b5bbb4e1ddcffd3962b43304ad10baffb9
Chethan Radhakrishna <chethanmr.2694@gmail.com>
2017-07-19 17:50:58 +0530

    Chat Bot bug fixes
    Added clear-message functionality

--- src/Components/ChatBot.js
+++ src/Components/ChatBot.js
@@ -1,25 +1,105 @@
 import React from 'react';
 import { ChatRoom } from './ChatRoom';
+import Reducer from '../Reducers';
+
 export class ChatBot extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { isShow: false }
+        this.state = { isShow: false, showDropDown: false, messages: [] }
+
         this.toggleChat = this.toggleChat.bind(this);
+        this.toggleDropDown = this.toggleDropDown.bind(this);
+        this.clearAllMessages = this.clearAllMessages.bind(this);
+        this.fetchMessages = this.fetchMessages.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
-    toggleChat(){
+    componentDidMount() {
+
+    }
+
+    fetchMessages() {
+        fetch('http://localhost:3100')
+            .then(res => {
+                return res.json();
+            }).then(data => {
+                this.setState({ messages: data });
+                var objDiv = document.getElementById("style-2");
+                objDiv.scrollTop = objDiv.scrollHeight;
+            });
+
+        this.forceUpdate();
+    }
+
+    handleKeyPress(postData) {
+        /*console.log(this.refs.text.value);
+        var postData = {
+           
+            MessageText: this.refs.text.value,
+            MessageAT: Date.now()
+        };
+        console.log(postData);
+        if (event != null && event.key == 'Enter') {*/
+        fetch('http://localhost:3100', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(postData)
+        })
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                //alert(data);
+                this.fetchMessages();
+
+            })
+
+        //this.setState({ messages: this.state.messages.push(postData) });
+
+        //}
+    }
+
+    toggleChat() {
         console.log('inside toggle');
-        this.setState({isShow: !this.state.isShow});
+        this.setState({ isShow: !this.state.isShow });
     }
+
+    toggleDropDown() {
+        console.log('inside drop-toggle');
+        this.setState({ showDropDown: !this.state.showDropDown });
+    }
+
+    clearAllMessages() {
+        fetch('http://localhost:3100/clear')
+            .then(res => {
+                return res.json();
+            }).then(data => {
+                //alert(data);
+                Reducer.message.dispatch({ type: 'ADD_MESSAGE', message: data });
+                this.setState({ showDropDown: !this.state.showDropDown });
+                this.fetchMessages();
+            });
+    }
+
     render() {
         var botClass = "";
         var roomClass = "";
+        var dropClass = "";
+        var menuClass = "";
         if (this.state.isShow) {
             botClass = "chatbot-maximize";
             roomClass = "chat-room-show";
+            menuClass = "menu-label-show";
         }
-        else{
-             botClass = "chatbot-minimize";
+        else {
+            botClass = "chatbot-minimize";
             roomClass = "chat-room-hide";
+            menuClass = "menu-label-hide";
+        }
+        if (this.state.showDropDown) {
+            dropClass = "chat-drop-show";
+        }
+        else {
+            dropClass = "chat-drop-hide";
         }
         return (
             <div>
@@ -28,13 +108,19 @@ export class ChatBot extends React.Component {
                         <svg width="200px" height="50px">
                             <rect x="0" y="0" rx="20" ry="20" width="160" height="40" className="chatbot-rect" />
                             Sorry, your browser does not support inline SVG.
-                        <text x="30" y="21">Chat </text>
+                        <text x="30" y="21">Chat     </text>
                         </svg>
                     </g>
 
                 </div>
+                <div className={menuClass}>
+                    <label htmlFor=""><span className="fa fa-bars" onClick={this.toggleDropDown}></span></label>
+                </div>
+                <div className={dropClass}>
+                    <label htmlFor="" onClick={this.clearAllMessages}>Clear All</label>
+                </div>
                 <div>
-                    <ChatRoom className={roomClass} />
+                    <ChatRoom className={roomClass} handleKeyPress={this.handleKeyPress} messages={this.state.messages} />
                 </div>
             </div>
         );
--- src/Components/ChatRoom.js
+++ src/Components/ChatRoom.js
@@ -4,13 +4,15 @@ export class ChatRoom extends React.Component {
     constructor(props) {
         super(props);
         this.state = { messages: [] }
-        this.fetchMessages = this.fetchMessages.bind(this);
+        //this.fetchMessages = this.fetchMessages.bind(this);
         this.handleKeyPress = this.handleKeyPress.bind(this);
     }
-    componentWillMount() {
-        this.fetchMessages();
+    componentDidMount() {
+       
+        // this.fetchMessages();
     }
-    fetchMessages() {
+
+    /*fetchMessages() {
         fetch('http://localhost:3100')
             .then(res => {
                 return res.json();
@@ -21,31 +23,34 @@ export class ChatRoom extends React.Component {
             });
 
         this.forceUpdate();
-    }
+    }*/
     handleKeyPress(event) {
         console.log(this.refs.text.value);
         var postData = {
-            MessageId: 1103,
+
             MessageText: this.refs.text.value,
             MessageAT: Date.now()
         };
         console.log(postData);
+
         if (event != null && event.key == 'Enter') {
-            fetch('http://localhost:3100', {
-                method: 'POST',
-                headers: { 'content-type': 'application/json' },
-                body: JSON.stringify(postData)
-            })
-                .then(res => {
-                    return res.json();
-                })
-                .then(data => {
-                    //alert(data);
-                    this.fetchMessages();
-
-                })
-
-            //this.setState({ messages: this.state.messages.push(postData) });
+            this.props.handleKeyPress(postData);
+             this.refs.text.value = "";
+            /* fetch('http://localhost:3100', {
+                 method: 'POST',
+                 headers: { 'content-type': 'application/json' },
+                 body: JSON.stringify(postData)
+             })
+                 .then(res => {
+                     return res.json();
+                 })
+                 .then(data => {
+                     //alert(data);
+                     this.fetchMessages();
+ 
+                 })
+ 
+             //this.setState({ messages: this.state.messages.push(postData) });*/
 
         }
     }
@@ -53,7 +58,7 @@ export class ChatRoom extends React.Component {
         //this.fetchMessages();
         //var className = this.props.show?"chat-room-show":"chat-room-hide";
         var messages = [];
-        messages = this.state.messages;
+        messages = this.props.messages;
         messages.map(message => {
             if (new Date(message.MessageAT) != 'Invalid Date' && new Date(message.MessageAT).getDay() == new Date().getDay()) {
                 message.MessageAT = new Date(message.MessageAT).toLocaleTimeString();
